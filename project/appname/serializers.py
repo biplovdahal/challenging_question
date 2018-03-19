b24cc5b3682589d02452eac2087731b117f43fc1
biplovdahal <biplovdahal@Biplovs-MacBook-Pro.local>
2018-03-19 15:53:50 -0700

    added serializer method field for roles

--- project/appname/serializers.py
+++ project/appname/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Member
  
+
+
+
+
 class MemberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Member
-        fields = '__all__'
+	roles = serializers.SerializerMethodField()
+
+
+	class Meta:
+		model = Member
+		fields = ('__all__')
+
+	def get_roles(self,obj):
+		return obj.get_role_display()
+
